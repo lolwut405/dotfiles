0670aa55e6433e7c3ddb3fa9434ca0025d5d33e2
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-04-02 20:19:22 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -21,13 +21,13 @@
     neofetch 
     vim
     
+    # Less minimal
     gitMinimal
     p7zip 
     unzip 
     wget 
     zip
-    #earlyoom
-  
+      
     # GUI
     #firefox 
     #mpv 
@@ -40,28 +40,33 @@
     #meld
 	
     # Plasma
-    #ark gnome-breeze spectacle
+    #ark 
+    #gnome-breeze 
+    #spectacle
     #plasma-nm plasma-pa
-  ];
-
-  fonts.fonts = with pkgs; [
-    dejavu_fonts
-    noto-fonts
-    liberation_ttf
-    source-code-pro    
+    #earlyoom
   ];
   
+  ### UNCOMMENT THE NEXT SECTIONS FOR  WHEN READY ###
   # Xorg
-  # services.xserver.enable = true; #will install packages
+  # services.xserver.enable = true; #auto-installs Xorg
   # services.xserver.layout = "us";
   # services.xkbOptions = "caps:swapescape";
   
   # KDE
-  # services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true; #auto-installs KDE
   # services.xserver.desktopManager.xterm.enable = false;
   # services.xserver.displayManager.sddm.enable = true;
   # environment.variables = { TERM = "konsole-256color"; };
 
+  # Fonts
+  # fonts.fonts = with pkgs; [
+  #   dejavu_fonts
+  #   noto-fonts
+  #   liberation_ttf
+  #   source-code-pro    
+  # ];
+  
   # Services
   # services.earlyoom.service = true;
   # services.openssh.enable = true;
@@ -70,47 +75,43 @@
   # Services for external mounting
   # services.udisks2.enable = true;
   # services.devmon.enable = true;
-  
-  # Security
-  security.rngd.enable = false; # vm only
-  security.sudo.wheelNeedsPassword = false;
-  
+    
   # Sound
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
 
   # Disable Mouse acceleration
-  services.xserver.config = ''
-    Section "InputClass"
-      Identifier "mouse accel"
-      Driver "libinput"
-      MatchIsPointer "on"
-      Option "AccelProfile" "flat"
-      Option "AccelSpeed" "0"
-    EndSection
-  '';
-
-  # Kernel
-  boot.kernel.sysctl = { "vm.swappiness" = 5;};
-  boot.kernel.sysctl = { "vm.vfs_cache_pressure" = 50;};
+  # services.xserver.config = ''
+  #   Section "InputClass"
+  #     Identifier "mouse accel"
+  #     Driver "libinput"
+  #     MatchIsPointer "on"
+  #     Option "AccelProfile" "flat"
+  #     Option "AccelSpeed" "0"
+  #   EndSection
+  # '';
 
   # Enable CUPS
   # services.printing.enable = true;
   # services.printing.drivers = with pkgs; [ <BLAH> ];
 
-  # Laptop
-  # networking.wireless.enable = true;
-  # services.xserver.libinput.enable = true;
-
   # Open ports in the firewall
-  # networking.firewall.allowedTCPPorts = [ ];
+  # networking.firewall.allowedTCPPorts = [ ];  #22 wont need opened
   # networking.firewall.allowedUDPPorts = [ ];
   # Or disable the firewall altogether
   # networking.firewall.enable = false;
 
-  # nixpkgs.config = {
-  #   allowUnfree = true;
-  # };
-
+  # Laptop
+  # nixpkgs.config.allowUnfree = true;
+  # networking.wireless.enable = true;
+  # services.xserver.libinput.enable = true;
+  
+  # System
+  boot.kernel.sysctl = { "vm.swappiness" = 5;};
+  boot.kernel.sysctl = { "vm.vfs_cache_pressure" = 50;};
+  security.rngd.enable = false; # vm only
+  security.sudo.wheelNeedsPassword = false;
+  
+  # System level
   system.stateVersion = "19.09";
 }
