a087459aef7c76cee6e4dc58a8b6facd183feace
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-04-06 06:20:10 +0000

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -25,7 +25,7 @@ printf 'hostonly="yes" \ncompress="pigz"' >> /mnt/etc/dracut.conf.d/custom.conf
 rm -f /etc/yum.repos.d/*{*cisco*,*test*,*modular*}*
 dnf install -y --installroot=/mnt --releasever=32 --nodocs \
 @core glibc-langpack-en grub2-pc htop kernel xfsprogs zram \
---exclude=firewalld,geolite2-*,gnome-keyring,NetworkManager,plymouth,selinux-*,sssd-* #openssh-server
+--exclude=firewalld,geolite2-*,gnome-keyring,NetworkManager,openssh-server,plymouth,selinux-*,sssd-*
 
 #dnf install -y --installroot=/mnt --releasever=32 --setopt=install_weak_deps=False --nodocs \
 #dracut glibc-langpack-en rootfiles systemd systemd-udev  \
