e22a8b93d18007e8fc592c9cb02c002646919d42
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-04-07 06:44:04 +0000

    Update fedora-install.sh

--- fedora-install.sh
+++ fedora-install.sh
@@ -8,8 +8,8 @@ set -x  #echo on
 # Partioning. msdos layout. Do lsblk first. cfdisk as an alternative
 wipefs -a /dev/sda
 parted /dev/sda -- mklabel msdos
-parted /dev/sda -- mkpart primary 1Mib 100% set 1 boot on
-mkfs.xfs /dev/sda1
+parted /dev/sda -- mkpart primary 1Mib 100%
+mkfs.xfs /dev/sda1 -f
 mount /dev/sda1 /mnt
 
 # Mount temp filesystems (for Dracut and Chroot)
