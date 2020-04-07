923e47fd366a8bd95b5517b2a92446c9e39507b3
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-04-07 06:05:32 +0000

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -46,6 +46,7 @@ flatpak install -y flathub org.fedoraproject.MediaWriter #no platform
 # Laptop
 #dnf install -y broadcom-wl
 #echo 'set-card-profile 0 output:hdmi-stereo' >> /etc/pulse/default.pa
+#https://wiki.archlinux.org/index.php/PulseAudio/Examples#Automatically_switch_audio_to_HDMI
 
 # Done
 rm JvkWH
