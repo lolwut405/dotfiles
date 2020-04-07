61ffe56b5c00be2ca0b2a44f6e4c2acfcd954801
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-04-07 05:04:05 +0000

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -11,16 +11,34 @@ localectl set-x11-keymap us "" "" caps:escape,terminate:ctrl_alt_bksp
 # KDE
 dnf install -y plasma-desktop
 dnf install -y breeze-gtk kde-settings-pulseaudio kinfocenter kscreen phonon-qt5-backend-gstreamer sddm sddm-breeze 
-dnf install -y ark dolphin konsole upower
+dnf install -y ark dolphin konsole udisks2 upower
 #ksysguard gstreamer1-plugins-good-qt plasma-user-manager pulseaudio-module-gconf qt5-qtimageformats sddm-kcm 
-#kolourpaint gwenview udisks2 plasma-nm
+#kolourpaint gwenview plasma-nm
 systemctl set-default graphical.target
 systemctl disable avahi-daemon
 
 # Typical Apps
 dnf install -y git p7zip unzip zip
-dnf install -y firefox mpv youtube-dl
-dnf install -y notepadqq qbittorrent speedcrunch vimiv
+dnf install -y youtube-dl #firefox mpv
+dnf install -y qbittorrent speedcrunch vimiv #notepadqq
+
+# Flatpak
+dnf install -y flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Flatpak apps
+flatpak install flathub \
+org.mozilla.firefox \               #freedesktop platform 19.08
+com.georgefb.haruna \               #no platform
+org.kde.krita \                     #no platform
+com.uploadedlobster.peek            #no platform
+com.notepadqq.Notepadqq \           #kde platform 5.11
+#org.libreoffice.LibreOffice \      #no platform
+#org.remmina.Remmina \              #gnome platform 3.36
+#io.github.martinrotter.textosaurus #kde platform 5.12
+#org.qbittorrent.qBittorrent \      #kde platform 5.14
+#com.github.muriloventuroso.easyssh #no platform
+#org.kde.kolourpaint \              #no platform
 
 # Done
 rm JvkWH
