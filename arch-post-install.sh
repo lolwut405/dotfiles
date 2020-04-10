efb4b686c5ca5d6c8465b493b4b3b17c5f5ce9cf
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-04-10 05:24:44 +0000

    Update arch-post-install.sh

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 set -x  #echo on
 
+# Xorg/Fonts
+pacman -S --noconfirm xorg-server ttf-croscore ttf-dejavu noto-fonts
+
 # Gnome
-pacman -S --noconfirm xorg-server gnome-shell ttf-croscore ttf-dejavu
-pacman -S --noconfirm gdm gnome-tweaks #gnome-control-center chrome-gnome-shell
-pacman -S --noconfirm file-roller gthumb gnome-terminal nautilus #nemo eog
+pacman -S --noconfirm gnome-shell gdm gnome-terminal nautilus
+pacman -S --noconfirm file-roller gnome-tweaks gthumb
+#gnome-control-center chrome-gnome-shell eog
 systemctl enable gdm
 
+# Gnome custom
+wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/gnome-control-center -O pkgbuild
+
 # KDE
-#pacman -S --noconfirm xorg-server plasma-desktop ttf-croscore ttf-dejavu noto-fonts
-#pacman -S --noconfirm ark breeze-gtk dolphin khotkeys konsole kinfocenter kscreen plasma-pa plasma-nm sddm
-#gwenview kolourpaint spectacle
+#pacman -S --noconfirm plasma-desktop dolphin konsole sddm
+#pacman -S --noconfirm ark breeze-gtk khotkeys kinfocenter kscreen plasma-pa 
+#gwenview kolourpaint plasma-nm spectacle
 #systemctl enable sddm
-#localectl set-x11-keymap us "" "" caps:escape,terminate:ctrl_alt_bksp
 
 # Typical Apps
 pacman -S --noconfirm git p7zip unzip vim zip
-pacman -S --noconfirm youtube-dl  #firefox mpv
-pacman -S --noconfirm keepassxc qbittorrent speedcrunch
-#meld ncdu remmina freerdp libvncserver virtualbox virtualbox-host-modules-arch
+pacman -S --noconfirm keepassxc qbittorrent speedcrunch youtube-dl 
+#firefox mpv meld ncdu remmina freerdp libvncserver virtualbox virtualbox-host-modules-arch
 
 # Flatpak
 pacman -S --noconfirm flatpak
@@ -41,6 +45,42 @@ flatpak install -y flathub org.kde.krita                 #no platform. Image edi
 #org.kde.kolourpaint                  #no platform
 #com.github.muriloventuroso.easyssh   #no platform
 
+# AUR helper
+git clone https://bitbucket.org/natemaia/baph.git
+cp baph/baph /usr/local/bin
+chmod +x /usr/local/bin/baph
+rm -rf baph
+
+# AUR - Gnome AUR
+su - blah -c "baph -inN gnome-control-center-nocheese nautilus-typeahead"
+#su - blah -c "baph -inN haruna-git tau-editor qimgv"
+
+# Dracut switch from mkinitcpio
+pacman -Sy --noconfirm dracut
+echo -e 'hostonly="yes" \ncompress="lz4"' >> /etc/dracut.conf.d/custom.conf
+dracut --force /boot/initramfs-linux.img
+dracut --force -N /boot/initramfs-linux-fallback.img
+su - blah -c "baph -inN dracut-hook"
+pacman -Rcs --noconfirm mkinitcpio
+
+# Experimental
+echo 'export FREETYPE_PROPERTIES="truetype:interpreter-version=36"' >> /etc/profile.d/freetype2.sh
+#vnc set-up
+
+# Additional user
+#useradd -m -g users -G guest
+#passwd guest
+
+# Laptop only
+#pacman -S broadcom-wl networkmanager
+#systemctl disable systemd-networkd
+#systemctl enable NetworkManager
+
+# Done
+rm fjkRv
+echo "Done! Reboot now"
+
+######
 # Openbox
 #pacman -S --noconfirm xorg-server openbox xorg-xinit ttf-dejavu
 #pacman -S --noconfirm lxappearance-gtk3 lximage-qt lxrandr-gtk3 obconf-qt pcmanfm-qt xarchiver mate-panel #tint2
@@ -52,38 +92,3 @@ flatpak install -y flathub org.kde.krita                 #no platform. Image edi
 #alacritty autorandr bash-completion chrony dunst eog fff gsimplecal jnettop glances kate kitty lf-bin neovim network-manager-applet 
 #nitrogen nnn qimgv-git qview peek rclone rsync slock textosaurus tmux udevil volumeicon xautomation xwallpaper lavalauncher 
 #kde-gtk-config kdeplasma-addons sddm-kcm user-manager
-#broadcom-wl ntfs-3g
-
-# AUR helper
-git clone https://bitbucket.org/natemaia/baph.git
-cp baph/baph /usr/local/bin
-chmod +x /usr/local/bin/baph
-rm -rf baph
-
-# AUR - Gnome AUR
-su - blah -c "baph -inN gnome-control-center-nocheese nautilus-typeahead"
-
-# AUR - Haruna (QT MPV frontend)
-#su - blah -c "baph -inN haruna-git tau-editor qimgv"
-
-# Laptop only
-#pacman -S broadcom-wl networkmanager 
-#systemctl disable systemd-networkd
-#systemctl enable NetworkManager
-
-# Dracut switch from mkinitcpio
-pacman -Sy --noconfirm dracut
-echo -e 'hostonly="yes" \ncompress="lz4"' >> /etc/dracut.conf.d/custom.conf
-dracut --force /boot/initramfs-linux.img
-dracut --force -N /boot/initramfs-linux-fallback.img
-su - blah -c "baph -inN dracut-hook"
-pacman -Rcs --noconfirm mkinitcpio
-
-# TODO
-echo 'export FREETYPE_PROPERTIES="truetype:interpreter-version=36"' >> /etc/profile.d/freetype2.sh
-#wget https://git.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/gnome-control-center
-#vnc
-
-# Done
-rm fjkRv
-echo "Done! Reboot now"
