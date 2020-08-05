c3451964d335cabac25c9b2897582a6dcf7d4b80
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-08-05 03:46:26 +0000

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -6,17 +6,18 @@ set -x  #echo on
 wipefs -a /dev/sda
 parted /dev/sda -- mklabel msdos
 parted /dev/sda -- mkpart primary 1Mib 100%
-##mkfs.xfs /dev/sda1 -f
-mkfs.btrfs /dev/sda1 -f
+mkfs.xfs /dev/sda1 -f
+#mkfs.btrfs /dev/sda1 -f
 mount /dev/sda1 /mnt
 
 # Install
 #echo "Server = http://mirrors.acm.wpi.edu/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-#echo "Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-#echo "Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-curl -o /etc/pacman.d/mirrorlist "https://www.archlinux.org/mirrorlist/?country=US&protocol=http"
-sed -i 's/#Server/Server/g' /etc/pacman.d/mirrorlist
-pacman -Syyu
+echo "Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+echo "Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+#curl -o /etc/pacman.d/mirrorlist "https://www.archlinux.org/mirrorlist/?country=US&protocol=http"
+#sed -i 's/#Server/Server/g' /etc/pacman.d/mirrorlist
+#pacman -Syy
+pacman -Syy archlinux-keyring
 pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget xfsprogs earlyoom networkmanager systemd-swap dbus-broker
 genfstab -U /mnt >> /mnt/etc/fstab
 
