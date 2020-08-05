dcdea5c458720931a1bae3ec6029b33f32aa5cb2
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-08-05 04:57:31 +0000

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -6,17 +6,14 @@ set -x  #echo on
 wipefs -a /dev/sda
 parted /dev/sda -- mklabel msdos
 parted /dev/sda -- mkpart primary 1Mib 100%
-mkfs.btrfs /dev/sda1 -f #mkfs.xfs /dev/sda1 -f
+mkfs.btrfs /dev/sda1 -f
 mount /dev/sda1 /mnt
 
 # Install
 echo "Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 echo "Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-#ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-#timedatectl set-ntp true
-#pacman -Sy archlinux-keyring --noconfirm
 sed -i "s/Required DatabaseOptional/Never/" /etc/pacman.conf
-pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget earlyoom networkmanager systemd-swap dbus-broker btrfs-progs #xfsprogs
+pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget networkmanager systemd-swap dbus-broker btrfs-progs #xfsprogs earlyoom
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Grub
@@ -30,23 +27,23 @@ chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 systemd-firstboot --root=/mnt --locale=en_US.UTF-8 --keymap=us --timezone=America/New_York --hostname=vm --setup-machine-id
 
 # NetworkManager
-systemctl enable networkmanager --root=/mnt
+systemctl enable NetworkManager --root=/mnt
 printf '[connectivity]\nuri=' > /mnt/etc/NetworkManager/conf.d/20-connectivity.conf
 
 # Other services
-systemctl enable earlyoom --root=/mnt
+#systemctl enable earlyoom --root=/mnt
 systemctl enable systemd-timesyncd --root=/mnt
 #systemctl mask systemd-homed systemd-userdbd.{service,socket} --root=/mnt
 
 # Swap
 systemctl enable systemd-swap --root=/mnt
 printf 'vm.swappiness = 5 \nvm.vfs_cache_pressure = 50' >> /mnt/etc/sysctl.d/99-sysctl.conf
-printf 'zswap_enabled=0 \nzram_enabled=1 \nzram_size=1G' >> /mnt/etc/systemd/swap.conf.d/10-swap.conf
+printf 'zswap_enabled=0 \nzram_enabled=1 \nzram_size=$(( RAM_SIZE / 8))' >> /mnt/etc/systemd/swap.conf
 
 # Arch specific tweaks
 echo 'en_US.UTF-8 UTF-8' > /mnt/etc/locale.gen
 chroot /mnt locale-gen
-systemctl mask lvm2-lvmetad.{service,socket} --root=/mnt
+#systemctl mask lvm2-lvmetad.{service,socket} --root=/mnt
 echo 'ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0|1", ATTR{queue/scheduler}="bfq"' > /mnt/etc/udev/rules.d/60-ioschedulers.rules
 systemctl enable dbus-broker --root=/mnt
 systemctl --global enable dbus-broker --root=/mnt
