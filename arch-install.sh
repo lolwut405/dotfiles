2e226e8d3bb6fb6025143d4ca8600d1db4306951
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2020-08-05 04:00:35 +0000

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -6,20 +6,14 @@ set -x  #echo on
 wipefs -a /dev/sda
 parted /dev/sda -- mklabel msdos
 parted /dev/sda -- mkpart primary 1Mib 100%
-#mkfs.xfs /dev/sda1 -f
-mkfs.btrfs /dev/sda1 -f
+mkfs.btrfs /dev/sda1 -f #mkfs.xfs /dev/sda1 -f
 mount /dev/sda1 /mnt
 
 # Install
-#echo "Server = http://mirrors.acm.wpi.edu/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 echo "Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 echo "Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-#curl -o /etc/pacman.d/mirrorlist "https://www.archlinux.org/mirrorlist/?country=US&protocol=http"
-#sed -i 's/#Server/Server/g' /etc/pacman.d/mirrorlist
-#pacman -Syy
 pacman -Sy archlinux-keyring --noconfirm
-pacman -S
-pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget xfsprogs earlyoom networkmanager systemd-swap dbus-broker
+pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget earlyoom networkmanager systemd-swap dbus-broker #xfsprogs
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Grub
@@ -30,7 +24,7 @@ chroot /mnt grub-install /dev/sda
 chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # Systemd-firstboot
-systemd-firstboot --root=/mnt --locale=en_US.UTF-8 --keymap=us --timezone=America/New_York --hostname=arch --setup-machine-id
+systemd-firstboot --root=/mnt --locale=en_US.UTF-8 --keymap=us --timezone=America/New_York --hostname=vm --setup-machine-id
 
 # NetworkManager
 systemctl enable networkmanager --root=/mnt
@@ -51,8 +45,8 @@ echo 'en_US.UTF-8 UTF-8' > /mnt/etc/locale.gen
 chroot /mnt locale-gen
 systemctl mask lvm2-lvmetad.{service,socket} --root=/mnt
 echo 'ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0|1", ATTR{queue/scheduler}="bfq"' > /mnt/etc/udev/rules.d/60-ioschedulers.rules
-#systemctl enable dbus-broker --root=/mnt
-#systemctl --global enable dbus-broker --root=/mnt
+systemctl enable dbus-broker --root=/mnt
+systemctl --global enable dbus-broker --root=/mnt
 
 # User account
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /mnt/etc/sudoers.d/wheel
