66b23d841ba7656ea993e6c88de1d814919486ca
lolwut405 <8081739+lolwut405@users.noreply.github.com>
2021-07-05 18:12:40 -0400

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -13,7 +13,7 @@ mount /dev/sda1 /mnt
 echo "Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 echo "Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
 timedatectl set-timezone America/New_York; timedatectl set-ntp true
-pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget networkmanager btrfs-progs #dbus-broker systemd-swap earlyoom xfsprogs 
+pacstrap /mnt base base-devel linux linux-firmware grub htop openssh sudo vi vim wget btrfs-progs zram-generator #dbus-broker 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Grub
@@ -26,17 +26,22 @@ chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 # Systemd-firstboot
 systemd-firstboot --root=/mnt --locale=en_US.UTF-8 --keymap=us --timezone=America/New_York --hostname=vm --setup-machine-id
 
-# NetworkManager
-systemctl enable NetworkManager --root=/mnt
-printf '[connectivity]\nuri=' > /mnt/etc/NetworkManager/conf.d/20-connectivity.conf
+# Systemd-networkd
+systemctl enable systemd-networkd --root=/mnt
+printf '[Match] \nName=en* \n[Network] \nDHCP=ipv4' > /mnt/etc/systemd/network/10-wired.network
 
-# Other services
-#systemctl enable earlyoom --root=/mnt
+# Systemd-resolved
+systemctl enable systemd-resolved --root=/mnt
+chroot /mnt ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+# Systemd-timesyncd
 systemctl enable systemd-timesyncd --root=/mnt
-#systemctl mask systemd-homed systemd-userdbd.{service,socket} --root=/mnt
+
+# Systemd-oomd
+systemctl enable systemd-oomd --root=/mnt
 
 # Swap
-#systemctl enable systemd-swap --root=/mnt
+
 printf 'vm.swappiness = 5 \nvm.vfs_cache_pressure = 50' >> /mnt/etc/sysctl.d/99-sysctl.conf
 #printf 'zswap_enabled=0 \nzram_enabled=1 \nzram_size=$(( RAM_SIZE / 8))' >> /mnt/etc/systemd/swap.conf
 
@@ -52,3 +57,14 @@ echo 'ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0|1", AT
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /mnt/etc/sudoers.d/wheel
 chroot /mnt useradd -m -g users -G wheel blah
 chroot /mnt passwd blah
+
+# NetworkManager
+#pacman -S networkmanager
+#systemctl enable NetworkManager --root=/mnt
+#printf '[connectivity]\nuri=' > /mnt/etc/NetworkManager/conf.d/20-connectivity.conf
+
+# Other services
+#systemctl mask systemd-homed systemd-userdbd.{service,socket} --root=/mnt
+
+# Swap
+#systemctl enable systemd-swap --root=/mnt
